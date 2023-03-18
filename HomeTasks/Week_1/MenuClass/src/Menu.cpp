b4d5540c016c14d0aa07f4299a1b39a4c87b7e31
Dmitry Morozov <9984398@gmail.com>
2023-03-18 10:00:07 +0300

    Initial Commit

---
+++ HomeTasks/Week_1/MenuClass/src/Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dmitry Morozov on 18/3/23.
+//
+
+#include "../include/Menu.hpp"
---
+++ HomeTasks/Week_1/MenuClass/src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Dmitry Morozov on 18/3/23.
+//
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "Hello, Week1 / MenuClass Demo" << std::endl;
+
+	return 0;
+
+}
